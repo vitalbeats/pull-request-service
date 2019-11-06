2413ada2443850ed122f5a021414b97c138c4498
Stephen Badger <stephen.badger@vitalbeats.com>
2019-11-06 16:00:54 +0100

    SCAUT-115: Print out pull requests we receive.

--- pull_request_service/payload.py
+++ pull_request_service/payload.py
@@ -7,12 +7,13 @@ from pull_request_service import webhook_token
 class PayloadHandler(resource.Resource):
     isLeaf = True
     secret_path = 'config/secret/webhook'
-    
+
     def __init__(self, path):
         secret_path = path
-    
+
     def render_POST(self, request):
         secret = request.getHeader('X-Hub-Signature')
+        event = request.getHeader('X-GitHub-Event')
         if secret:
             wht = webhook_token.WebHookToken(self.secret_path)
             content = request.content.read()
@@ -20,6 +21,9 @@ class PayloadHandler(resource.Resource):
             sig_parts = secret.split('=', 1)
             if len(sig_parts) > 1 and hmac.compare_digest(sig_parts[1], digest):
                 content_as_json = json.loads(content)
+                if event == 'pull_request':
+                    project = content_as_json['repository']['name'].lower() + '-pr-' + content_as_json['number']
+                    print('Received pull request notification for ' + project + ' with action ' + content_as_json['action'])
                 return content
         request.setResponseCode(403)
         return 'Forbidden.'.encode('utf-8')
