4bd3457162a56d2094d9f53f0d51252aa28a2490
Stephen Badger <stephen.badger@vitalbeats.com>
2019-11-07 13:23:18 +0100

    SCAUT-115: Add internal kubernetes service address.

--- pull_request_service/project.py
+++ pull_request_service/project.py
@@ -14,6 +14,7 @@ class ProjectLoader():
         # Openshift API tokens never expire, they can be revoked by deleting the associated secret though
         k8s_config.api_key = {"authorization": "Bearer " + token}
         k8s_config.ssl_ca_cert = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
+        k8s_config.host = 'https://kubernetes.default.svc:443'
         self.dyn_client = DynamicClient(client.ApiClient(configuration=k8s_config))
         self.project_list = dyn_client.resources.get(api_version='project.openshift.io/v1', kind='Project').get()
 
