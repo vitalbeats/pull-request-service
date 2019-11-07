f7d6891e3bf269327b19c288e9a78e6c0279e7b6
Stephen Badger <stephen.badger@vitalbeats.com>
2019-11-07 14:45:07 +0100

    SCAUT-115: Delete project when pull request is closed.

--- pull_request_service/project.py
+++ pull_request_service/project.py
@@ -4,7 +4,6 @@ from openshift.dynamic import DynamicClient
 
 class ProjectList():
     dyn_client = None
-    project_list = {}
 
     def __init__(self):
         k8s_config = client.configuration.Configuration()
@@ -16,7 +15,6 @@ class ProjectList():
         k8s_config.ssl_ca_cert = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
         k8s_config.host = 'https://kubernetes.default.svc:443'
         self.dyn_client = DynamicClient(client.ApiClient(configuration=k8s_config))
-        self.project_list = self.dyn_client.resources.get(api_version='project.openshift.io/v1', kind='Project')
 
     def delete(self, project_name):
-        self.project_list.delete(field_selector='metadata.name=' + project_name)
+        self.dyn_client.resources.delete(name=project_name, api_version='project.openshift.io/v1', kind='Project')
