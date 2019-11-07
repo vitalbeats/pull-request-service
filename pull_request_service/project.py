6f85c4e80c368fd7d3eff14f659d859cec66d772
Stephen Badger <stephen.badger@vitalbeats.com>
2019-11-07 13:15:40 +0100

    SCAUT-115: Read token inline.

--- pull_request_service/project.py
+++ pull_request_service/project.py
@@ -2,18 +2,17 @@ import yaml
 from kubernetes import client
 from openshift.dynamic import DynamicClient
 
-def __file_to_line(file_name):
-    with open(file_name, 'r') as f:
-        return f.read()
-
 class ProjectLoader():
     dyn_client = None
     project_list = {}
 
     def __init__(self):
         k8s_config = client.configuration.Configuration()
+        token = ''
+        with open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r') as f:
+            token = f.read()
         # Openshift API tokens never expire, they can be revoked by deleting the associated secret though
-        k8s_config.api_key = {"authorization": "Bearer " + __file_to_line('/var/run/secrets/kubernetes.io/serviceaccount/token')}
+        k8s_config.api_key = {"authorization": "Bearer " + token}
         k8s_config.ssl_ca_cert = '/var/run/secrets/kubernetes.io/serviceaccount/ca.crt'
         self.dyn_client = DynamicClient(client.ApiClient(configuration=k8s_config))
         self.project_list = dyn_client.resources.get(api_version='project.openshift.io/v1', kind='Project').get()
