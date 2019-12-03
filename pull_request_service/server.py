d2cee7c696dfe1ba1a0dcaa270b34789748391e6
Stephen Badger <stephen.badger@vitalbeats.com>
2019-12-03 12:12:01 +0000

    SCAUT-576: Fix missing import.

--- pull_request_service/server.py
+++ pull_request_service/server.py
@@ -2,6 +2,7 @@ from twisted.web import server, resource
 from twisted.internet import reactor
 from pull_request_service import payload
 from twisted.python import log
+import sys
 
 def main():
     log.startLogging(sys.stdout)
